5ebcc75c3ad8edf3b853fa07672d9a6af5475248
simonomi <thesimonpellerin@gmail.com>
2023-07-28 20:58:34 -0500

    changed format to be notes-based instead of words-based

--- database_generator.py
+++ database_generator.py
@@ -8,19 +8,26 @@ class Language:
 	def __init__(self, _name, _description):
 		self.name = _name
 		self.description = _description
-		self.new = []
+		self.notes = []
+	
+	def serialize(self):
+		self.notes = [x.__dict__ for x in self.notes]
+
+class Note:
+	def __init__(self, _name, _terms, _new):
+		self.name = _name
+		self.terms = _terms
+		self.new = _new
 
 flashcard_regex = compile(r"(.*?)(?:「.+」|（.+）|{(?:L|H)+})?:::?.*")
 comment_regex = compile(r"<!--[\w\W]*?(?:-->|$)")
+markup_regex = compile(r"(?P<delim>\*\*|\*|__|_)(?P<text>[^\*_]+)(?P=delim)")
 
-def remove_comments(string):
-	return comment_regex.sub("", string)
+def remove_comments_and_markup(string):
+	return markup_regex.sub(r"\g<text>", comment_regex.sub("", string))
 
 def is_not_読み(card):
 	return "読み" not in card
-
-def identity(x):
-	return x
 
 # MARK: config
 database_file = "languages.json"
@@ -34,7 +41,7 @@ languages = [
 	),
 	Language(
 		"русский", 
-		"i have... mixed feelings about russian. on one hand, coming from japanese, its wayyy more similar to english, making it (theoretically) easier to learn. on the other hand, how can one be expected to pronounce 12 different consonants in a row??? this language is so funky and i really just don't know how to feel about it."
+		"i have... mixed feelings about russian. on one hand, coming from japanese, its wayyy more similar to english, making it (theoretically) easier to learn. on the other hand, how can one be expected to pronounce 12 different consonants in a row??? this language is so silly and i really just don't know how to feel about it."
 	)
 ]
 
@@ -43,33 +50,51 @@ with open(database_file) as file:
 	old_languages = load(file)
 
 for language in languages:
-	language_notes = glob(obsidian_path + language.name + "/**/*.md", recursive=True)
+	language_files = glob(obsidian_path + language.name + "/**/*.md", recursive=True)
 	
 	old_language = [x for x in old_languages if x["name"] == language.name][0]
 	old_words = old_language["words"] + old_language["new"]
+	# old_words = [].join([x["terms"] + x["new"] for x in old_language["notes"]])
 	
-	language_words = []
-	for language_note in language_notes:
-		if language_note.split("/")[-1][:-3] in notes_to_skip: continue
+	for language_file in language_files:
+		note_name = language_file.split("/")[-1][:-3]
+		if note_name in notes_to_skip: continue
 		
-		with open(language_note) as file:
+		with open(language_file) as file:
 			is_active = file.readline().startswith(f"#{language.name}")
 			if not is_active: continue
 			
-			file_text = remove_comments(file.read())
+			file_text = remove_comments_and_markup(file.read())
 		
 		note_words = seq(file_text.split("\n"))\
 			.filter(is_not_読み)\
 			.map(flashcard_regex.match)\
-			.filter(identity)\
+			.filter(bool)\
 			.map(lambda match: match.group(1))
 		
-		language_words += note_words
-	
-	language.words = [word for word in language_words if word in old_words]
-	language.new   = [word for word in language_words if word not in old_words]
+		note_old_words = [x for x in note_words if x in old_words]
+		note_new_words = [x for x in note_words if x not in old_words]
+		
+		note = Note(
+			note_name,
+			note_old_words,
+			note_new_words
+		)
+		
+		language.notes.append(note)
+		if note.new:
+			print(f"new terms for {language.name}/{note.name}: [{', '.join(note.new)}]")
 
+[x.serialize() for x in languages]
 output_data = [x.__dict__ for x in languages]
+
+dont_save = input("save? ").lower().startswith("n")
+
+if dont_save:
+	print("cancelling")
+	exit()
+else:
+	print("saving")
 
 with open(database_file, "w") as file:
 	dump(output_data, file, ensure_ascii=False)
